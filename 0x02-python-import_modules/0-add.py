c41378e007aa6ec80a0535db3d86e75d85920a9e
Enaj-ciiru <jnjoroge.immaculate@gmail.com>
2022-06-07 09:23:44 +0300

    update

--- 0x02-python-import_modules/0-add.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
-    """
-    Prints the result of the addition between two numbers
-    """
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
