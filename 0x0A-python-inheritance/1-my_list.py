206d51796487269c151448702ebfd1525ea90123
Enaj-ciiru <jnjoroge.immaculate@gmail.com>
2022-07-04 16:00:13 +0300

    list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-
-""" Defines a class MyList that inherits from list """
+"""Module 1-my_list.
+Creates a class inheriting from the list class.
+"""
 
 
 class MyList(list):
     """
     Uses buitin list methods to print sorted list
     """
+
     def print_sorted(self):
         """ Prints a sorted list """
         print(sorted(self, reverse=False))
